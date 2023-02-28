616327a3f33dc76e0d6d147e02ee35dd8a30b71b
Juan José Ospina Erazo <101602868+JuanOspina05@users.noreply.github.com>
2023-02-27 22:55:24 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,38 +1,56 @@
-public class Control {
-    private TV tv;
-    
-    public void volumenUp(){
-        tv.volumenUp();
-    }
-    public void volumenDown(){
-        tv.volumenDown();
-    }
-    
-    public void canalUp(){
-        tv.canalUp();
-    }
-    public void canalDown(){
-        tv.canalDown();
-    }
-    
-    public void setCanal(int canal){
-        tv.setCanal(canal);
-    }
+package taller3.televisores;
 
-    public void turnOn(){
-        tv.turnOn();
-    }
-    public void turnOff(){
-        tv.turnOff();
-    }
-    public void enlazar(TV tv){
-        this.tv = tv;
-        tv.setControl(this);
-    }
-    public void setTv(TV tv){
-        this.tv = tv;
-    }
-    public TV getTv(){
-        return this.tv;
-    }
+public class Control {
+	TV tv;
+	
+	
+	
+	public TV getTv() {
+		return tv;
+	}
+	
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+	
+	public void canalUp() {
+		tv.canalUp();
+
+	}
+	
+	public void canalDown() {
+		tv.canalDown();
+	}
+	
+	
+	
+    public void enlazar(TV tv) {
+		this.tv = tv;
+		tv.control = this;
+		
+	}
+    
+	public void turnOn() {
+		tv.turnOn();
+		
+	}
+	
+	public void turnOff() {
+		tv.turnOff();	
+		
+	}
+	
+    public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+    
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+		
+	}
+
 }
